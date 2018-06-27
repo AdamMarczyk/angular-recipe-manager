d2995ed34487610e752c28c3659bbb1d4920cf91
Adam Marczyk <adam.marczyk90@gmail.com>
2018-06-27 18:19:56 +0200

    add recipe item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipesListComponent, RecipesDetailComponent],
+  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipesListComponent, RecipesDetailComponent, RecipeItemComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
